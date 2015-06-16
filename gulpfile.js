9037207ac3c4b317408f6bae75ae7fdb238e69e7
Joseph Tary <joseph@strongloop.com>
2015-06-16 09:28:53 -0700

    remove bundle fix npm script

--- gulpfile.js
+++ gulpfile.js
@@ -16,31 +16,11 @@ var DST_PATH = 'client/build/js';
 var STATIC_FILES = ['client/src/*.html', 'client/src/templates/*.html'];
 var SCRIPT_FILES = ['client/src/scripts/*.js'];
 
-function buildBundle() {
-  var appDir = 'client/src/scripts';
-  var b = bf({
-    basedir: appDir
-  });
-
-  // add the main application file
-  b.require('./loopback-app.js', { expose: 'loopback-app' });
-
-  // add boot instructions
-  boot.compileToBrowserify(appDir, b);
-
-  // create the bundle
-  var out = fs.createWriteStream('browser-bundle.js');
-  b.bundle().pipe(out);
-  // handle out.on('error') and out.on('close')
-}
-
 gulp.task('build', function() {
   gulp.src(WEBAPP_SRC)
     .pipe(browserify(BROWSERIFY_CONFIG))
     .pipe(concat(BUNDLE))
     .pipe(gulp.dest(DST_PATH));
-
-  buildBundle();
 });
 
 gulp.task('copy', function() {
