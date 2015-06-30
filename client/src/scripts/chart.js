cf210611ef1b043aa3667f93fab40bf414ceb4d2
Joseph Tary <joseph@strongloop.com>
2015-06-30 15:15:30 -0700

    Add axis for chart y-value
    Add an axis on the chart for the y-value

--- client/src/scripts/chart.js
+++ client/src/scripts/chart.js
@@ -15,6 +15,10 @@ module.exports = function() {
         .range([height, 0])
         .domain([0, 500]);
 
+      var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('right');
+
       var xScale = d3.scale.linear()
         .range([0, width])
         .domain([0, 99]);
@@ -23,6 +27,11 @@ module.exports = function() {
         .append('svg')
           .attr('height', height)
           .attr('width', width);
+
+      var yScaleGroup = svg.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(' + (width - 50) + ', 0)')
+        .call(yAxis);
 
       var line = d3.svg.line()
         .x(function(d, i) { return xScale(i); })
@@ -34,6 +43,12 @@ module.exports = function() {
         .attr('d', line);
 
       $scope.$watch('data', function(newVal) {
+        yScale.domain(d3.extent(newVal, function(d) {
+          return d.result;
+        })).nice();
+
+        yScaleGroup.call(yAxis.scale(yScale));
+
         if (newVal) {
           path.datum(newVal, function(d) { return d.index; })
             .attr('d', line);
