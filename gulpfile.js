2f4f371b150043e7d31a069e7aa5ca19348c431d
Joseph Tary <joseph@strongloop.com>
2015-07-02 16:06:38 -0700

    Fix some packaging issues
    Fix some version mismatches that break building the app.

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,11 @@ var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 
-var boot = require('loopback-boot');
-var bf = require('browserify');
-var fs = require('fs');
-
 var WEBAPP_SRC = 'client/src/scripts/app.js';
 var BROWSERIFY_CONFIG = {
   debug: true
 };
+
 var BUNDLE = 'bundle.js';
 var DST_PATH = 'client/build/js';
 var STATIC_FILES = ['client/src/*.html', 'client/src/templates/*.html'];
