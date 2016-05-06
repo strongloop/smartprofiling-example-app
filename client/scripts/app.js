6175cd95b87af5e8baf2de7b01dd97302a2ccd77
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 16:11:26 -0700

    update copyright notices and license

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: smartprofiling-example-app
+// US Government Users Restricted Rights - Use, duplication or disclosure
+// restricted by GSA ADP Schedule Contract with IBM Corp.
+
 var angular = require('angular-browserify');
 var app = angular.module('smartProfilerDemo', []);
 
--- client/src/scripts/chart.js
+++ client/src/scripts/chart.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: smartprofiling-example-app
+// US Government Users Restricted Rights - Use, duplication or disclosure
+// restricted by GSA ADP Schedule Contract with IBM Corp.
+
 var d3 = require('d3-browserify');
 
 module.exports = function() {
